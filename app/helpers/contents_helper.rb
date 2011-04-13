045569355fcdaef7ab8a0f1d1d17efa7d5909786
Mathieu <mathieu.rousseau.31@gmail.com>
2011-04-13 00:07:41 -0300

    get content homepage using content ancestry added links in the menu showing menu in all contents cleaned css

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,2 +1,10 @@
 module ContentsHelper
+  def getHomepage(content)
+    if(content.ancestry)
+      homepage_id = @content.ancestry.gsub(/\/.*/,"")
+      @homepage = Content.find(homepage_id)
+    else
+      @homepage = content
+    end
+  end
 end
