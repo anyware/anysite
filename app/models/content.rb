259565e36fb3e9a4b2354e29f97e619e188888f9
Mathieu <mathieu.rousseau.31@gmail.com>
2011-04-19 00:37:45 -0300

    contents default_scope order by position

--- app/models/content.rb
+++ app/models/content.rb
@@ -4,4 +4,5 @@ class Content < ActiveRecord::Base
   has_and_belongs_to_many :resources
   has_one :creator, :foreign_key => "created_by"
   has_one :editor, :foreign_key => "edited_by" 
+  default_scope :order => "position ASC"
 end
